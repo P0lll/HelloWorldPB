329c0de2a9407d019ff655c58537edfa46a3aa8d
Pavel <pbykatin@gmail.com>
2023-03-17 22:56:27 +0400

    Setup UI

--- HelloWorldPB/ViewController.swift
+++ HelloWorldPB/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let cornerRadius: CGFloat = 55
+    let numberOfAlpha = 0.3
+    let standartAlpha: CGFloat = 1
+    
+    @IBOutlet weak var RedLightView: UIView!
+    @IBOutlet weak var YellowLightView: UIView!
+    @IBOutlet weak var GreenLightView: UIView!
+    
+    @IBOutlet var ButtonDesigner: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        RedLightView.layer.cornerRadius = cornerRadius
+        YellowLightView.layer.cornerRadius = cornerRadius
+        GreenLightView.layer.cornerRadius = cornerRadius
+        
+        RedLightView.alpha = numberOfAlpha
+        YellowLightView.alpha = numberOfAlpha
+        GreenLightView.alpha = numberOfAlpha
+        
+        ButtonDesigner.layer.cornerRadius = 15
     }
+    
+    
+    @IBAction func StartStopActionButton(_ sender: UIButton) {
 
-
+        ButtonDesigner.setTitle("NEXT", for: .normal)
+        
+        if RedLightView.alpha == YellowLightView.alpha {
+            RedLightView.alpha = standartAlpha
+            GreenLightView.alpha = numberOfAlpha
+        }
+        else if RedLightView.alpha > numberOfAlpha
+                && YellowLightView.alpha == GreenLightView.alpha {
+            YellowLightView.alpha = standartAlpha
+            RedLightView.alpha = numberOfAlpha
+        }
+        else if YellowLightView.alpha > numberOfAlpha
+                    && GreenLightView.alpha == RedLightView.alpha {
+            GreenLightView.alpha = standartAlpha
+            YellowLightView.alpha = numberOfAlpha
+        }
+        
+    }
 }
-
